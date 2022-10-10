67e930503ea10c9c64a8d35336d1e8640090f69d
galihroswandi <111326692+galihroswandi@users.noreply.github.com>
2022-10-10 10:40:23 +0700

    video 15 tambah fitur tambah data

--- src/container/blogPost/BlogPost.jsx
+++ src/container/blogPost/BlogPost.jsx
@@ -1,46 +1,62 @@
 import React, { Component, Fragment } from 'react'
 import PostComponent from '../../components/post/PostComponent'
 import axios from 'axios'
-import "./BlogPost.css"
+import './BlogPost.css'
 
 class BlogPost extends Component {
   state = {
     post: [],
-    formBlogPost : {
-      userId : 1,
-      id : 1,
-      title : "",
-      body : ""
-    }
+    formBlogPost: {
+      userId: 1,
+      id: 1,
+      title: '',
+      body: '',
+    },
   }
 
   getDataAPI = () => {
-    axios.get('http://localhost:3004/posts').then((response) => {
+    axios.get('http://localhost:3004/posts?_sort=id&_order=desc').then((response) => {
       this.setState({
         post: response.data,
       })
     })
   }
 
+  postDataAPI = () => {
+    axios.post('http://localhost:3004/posts', this.state.formBlogPost).then(
+      (respose) => {
+        this.getDataAPI()
+      },
+      (err) => {
+        alert(err)
+      },
+    )
+  }
+
   handleRemove = (id) => {
-    console.log(id)
     axios.delete(`http://localhost:3004/posts/${id}`).then((response) => {
       this.getDataAPI()
     })
   }
 
   handleChangeForm = (e) => {
-    console.log(e.target)
-
-    let formBlogPostNew = {...this.state.formBlogPost};
-    formBlogPostNew[e.target.name] = e.target.value;
+    let formBlogPostNew = { ...this.state.formBlogPost }
+    formBlogPostNew[e.target.name] = e.target.value
 
     this.setState({
-      formBlogPost : formBlogPostNew
-    }, () => {
-      console.log("object : ", this.state.formBlogPost);
+      formBlogPost: formBlogPostNew,
     })
+  }
 
+  handleSubmit = () => {
+    if (
+      this.state.formBlogPost.title == '' ||
+      this.state.formBlogPost.body == ''
+    ) {
+      alert('title / deskripsi tidak boleh kosong !')
+    } else {
+      this.postDataAPI()
+    }
   }
 
   componentDidMount() {
@@ -67,10 +83,21 @@ class BlogPost extends Component {
           </div>
           <div className="content-form">
             <label htmlFor="title">Judul</label>
-            <input type="text" name="title" id="title" onChange={this.handleChangeForm} />
+            <input
+              type="text"
+              name="title"
+              id="title"
+              onChange={this.handleChangeForm}
+            />
             <label htmlFor="body">Deskripsi</label>
-            <textarea name="body" id="body" cols="30" rows="10" onChange={this.handleChangeForm}></textarea>
-            <button>Tambah Data</button>
+            <textarea
+              name="body"
+              id="body"
+              cols="30"
+              rows="10"
+              onChange={this.handleChangeForm}
+            ></textarea>
+            <button onClick={this.handleSubmit}>Tambah Data</button>
           </div>
         </div>
         <div
