4ddaecea6e6b8c0bf13d42f3885db3f16e0f9beb
Galih Roswandi <galihroswandi25@gmail.com>
2022-10-11 21:56:50 +0700

    video 23 memahami konsep redux(Dispatch & Subscription) Redux PART 2

--- redux.js
+++ redux.js
@@ -2,19 +2,38 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-    value : "1",
+    value : 1,
     age : 19
 }
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
-    return state;
+    switch( action.type ){
+        case "ADD_AGE" :
+            return {
+                ...state,
+                age : state.age + 1,
+            }
+        case "CHANGE_VALUE" : 
+            return{
+                ...state,
+                value : state.value + action.newValue
+            }
+        default :
+            return state;
+    }
 }
 
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-// Dispatch Action
-
 // Subscription
+store.subscribe(() => {
+    console.log("Store Change : ", store.getState());
+})
+
+// Dispatch Action
+store.dispatch({type : "ADD_AGE"});
+store.dispatch({type : "CHANGE_VALUE", newValue : 99})
+console.log(store.getState());
